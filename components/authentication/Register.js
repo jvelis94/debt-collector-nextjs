b06f74699e59b407abdd9605e3d537b57a5abcb6
Joel Velis <joelvelis94@gmail.com>
2021-09-09 20:44:37 -0400

    updated bill register to reroute to bills page;

--- components/authentication/Register.js
+++ components/authentication/Register.js
@@ -3,11 +3,13 @@ import Button from '@material-ui/core/Button';
 import styles from './AccessApp.module.css'
 import { useContext, useRef } from 'react';
 import AuthenticationContext from '../../store/authentication-store';
+import { useRouter } from 'next/router'
 
 const Register = (props) => {
     const ctx = useContext(AuthenticationContext)
     const emailRef = useRef()
     const passwordRef = useRef()
+    const router = useRouter()
 
 
     const handleSubmit = async (e) => {
